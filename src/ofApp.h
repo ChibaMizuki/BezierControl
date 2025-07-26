7d4c9337e996db74fc95122a8608c064934b5fb2
Lai <lokutouzan@gmail.com>
2025-07-26 16:42:11 +0900

    制御点をマウス操作で移動できるように

--- src/ofApp.h
+++ src/ofApp.h
@@ -4,13 +4,17 @@
 
 class ofApp : public ofBaseApp{
 	private:
-		vector<ofVec2f> ControlPoint;
-		int ControlNum;
+		vector<ofVec2f> ControlPoint; // 制御点の位置
+		int				ControlNum; // 制御点の個数
+		int				PointIndex = -1; // 制御点の番号取得（-1なら動作しない）
+		float			PointSize; // 点のサイズ
+		
 
-		float interval;
-		int width, height, widthOffset, heightOffset;
-		ofVec2f offset;
-		ofEasyCam camera;
+		float			interval; // ベジエの間隔
+		int				width, height, widthOffset, heightOffset; // 画面サイズ
+		int				endX; // 終点のx座標
+		ofVec2f			offset; // 画面サイズのオフセット
+		ofEasyCam		camera; 
 
 	public:
 		void setup();
